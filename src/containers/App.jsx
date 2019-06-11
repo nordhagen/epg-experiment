2d0413cfa5d5807be1e30ad8603bea48bf9def6c
nordhagen <oyvind.nordhagen@gmail.com>
2019-06-11 10:27:05 +0200

    add simple view switching

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import EPGListView from './EPGListView'
+import EPGDetailView from './EPGDetailView'
+
+const VIEW_MAP = {
+  'list'    : EPGListView,
+  'detail'  : EPGDetailView
+}
 
 class App extends React.Component {
+  state = {
+    currentView: 'list'
+  }
+
+  handleViewClick = (origin) => {
+    let newView = (origin === 'list')
+      ? 'detail'
+      : 'list'
+
+    this.setState({currentView:newView})
+  }
+
   render(){
+    let View = VIEW_MAP[this.state.currentView]
     return(
-      <h1>Hello from app!</h1>
+      <View handleClick={this.handleViewClick} />
     )
   }
 }
