614ea8d16446e435fa0d8392a501b5a95c5f8951
nordhagen <oyvind.nordhagen@gmail.com>
2019-06-11 16:08:29 +0200

    formatting

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,13 +4,13 @@ import EPGDetailView from './EPGDetailView'
 import styles from './App.scss'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { clockTick } from '../state/actions'
+import { clockTick, fetchEPG } from '../state/actions'
 
-const tickInterval = 0;
+const tickInterval = 0
 
 const VIEW_MAP = {
-  'list'    : EPGListView,
-  'detail'  : EPGDetailView
+  list: EPGListView,
+  detail: EPGDetailView
 }
 
 class App extends React.Component {
@@ -18,25 +18,24 @@ class App extends React.Component {
     currentView: 'list'
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.tickInterval = setInterval(this.props.clockTick, 1000)
+    this.props.fetchEPG()
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     clearInterval(this.tickInterval)
   }
 
-  handleViewClick = (origin) => {
-    let newView = (origin === 'list')
-      ? 'detail'
-      : 'list'
+  handleViewClick = origin => {
+    let newView = origin === 'list' ? 'detail' : 'list'
 
-    this.setState({currentView:newView})
+    this.setState({ currentView: newView })
   }
 
-  render(){
+  render() {
     let View = VIEW_MAP[this.state.currentView]
-    return(
+    return (
       <main className={styles.EPG}>
         <View handleClick={this.handleViewClick} />
       </main>
@@ -44,6 +43,10 @@ class App extends React.Component {
   }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ clockTick }, dispatch)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ clockTick, fetchEPG }, dispatch)
 
-export default connect(null, mapDispatchToProps)(App)
+export default connect(
+  null,
+  mapDispatchToProps
+)(App)
